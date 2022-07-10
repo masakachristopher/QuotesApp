28d8a89c86635db22124f71fa91b5d280616caf7
masakachristopher <masakachristopher@yahoo.com>
2022-07-10 20:08:42 +0300

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Christopher Masaka on 10/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
